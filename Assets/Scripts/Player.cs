e2468173f7b361255ff36887c857a4fa61abcee3
tulireps <tulireps@gmail.com>
2018-11-21 19:45:06 +0200

    liiguvad
    enemy attacks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	//members or variables
-	public float moveSpeed = 7.0f;	
+    //members or variables
+    public float health = 3.0f;
+	public float moveSpeed = 7.0f;
+    public float score = 0.0f;
 	
 	public GameObject bulletSpawner;
 	public GameObject bullet;
+    public Transform initial;
 
 
 	//methods or functions
@@ -45,7 +48,21 @@ public class Player : MonoBehaviour {
 			Shoot();
 		}	
 	}
-	void Shoot(){
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            health -= 1.0f;
+            transform.Translate(initial.position, Space.World);
+            if (health < 0)
+            {
+                print("Player died!");
+                //Destroy(this.gameObject);
+            }
+
+        }
+    }
+    void Shoot(){
 		Instantiate(bullet.transform, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
 	}
 }
